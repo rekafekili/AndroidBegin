858c09a9e6b55b70731afca79e1697a8d2804b86
rekafekili <rekafekili@gmail.com>
2020-01-20 20:02:24 +0900

    20200113 Android Week1

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("MainActivity", "onCreate()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    protected void onStart() {
+
+    }
+}
